98c6d1b09251bec169e95d54c5eb1ef77d48e1dc
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-06-19 12:35:50 -0700

    Convert the ThingDef override to a ModExtension, clean up comments

--- PlagueGun/Projectile_PlagueBullet.cs
+++ PlagueGun/Projectile_PlagueBullet.cs
@@ -9,16 +9,10 @@ namespace Plague
 {
     public class Projectile_PlagueBullet : Bullet
     {
-        #region Properties
-        public ThingDef_PlagueBullet Def
-        {
-            get
-            {
-                //Case sensitive! If you use Def it will return Def, which is this getter. This will cause a never ending cycle and a stack overflow.
-                return this.def as ThingDef_PlagueBullet;
-            }
-        }
-        #endregion
+        // This is a property, which is essentially a method without any arguments.
+        // The => notation is a simplified way to give it a getter without any setter.
+        // We're essentially aliasing base.def.GetModExtension<ModExtension_PlagueBullet>(), which is quite a mouthful, with a much shorter term.
+        public ModExtension_PlagueBullet Props => base.def.GetModExtension<ModExtension_PlagueBullet>();
 
         #region Overrides
         protected override void Impact(Thing hitThing)
@@ -35,10 +29,10 @@ namespace Plague
              * Make sure your code checks if things actually exist, before they
              * try to use the code that belongs to said things.
              */
-            if (Def != null && hitThing != null && hitThing is Pawn hitPawn) //Fancy way to declare a variable inside an if statement. - Thanks Erdelf.
+            if (Props != null && hitThing != null && hitThing is Pawn hitPawn) //Fancy way to declare a variable inside an if statement. - Thanks Erdelf.
             {
-                var rand = Rand.Value; // This is a random percentage between 0% and 100%
-                if (rand <= Def.AddHediffChance) // If the percentage falls under the chance, success!
+                float rand = Rand.Value; // This is a random percentage between 0% and 100%
+                if (rand <= Props.AddHediffChance) // If the percentage falls under the chance, success!
                 {
                     /*
                      * Messages.Message flashes a message on the top of the screen. 
@@ -52,32 +46,34 @@ namespace Plague
                         this.launcher.Label, hitPawn.Label
                     ), MessageTypeDefOf.NeutralEvent);
 
-                    //This checks to see if the character has a heal differential, or hediff on them already.
-                    var plagueOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Def.HediffToAdd);
-                    var randomSeverity = Rand.Range(0.15f, 0.30f);
+                    // This checks to see if the character has a health differential, or hediff, on them already.
+                    Hediff plagueOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Props.HediffToAdd);
+                    // This is an example of hardcoding a variable, which is generally bad practice. I've kept it from the original code as an example.
+                    // A good exercise to see whether you understand exposing variables to XML would be to add a FloatRange to the ModExtension and get a random value from it here.
+                    float randomSeverity = Rand.Range(0.15f, 0.30f);
                     if (plagueOnPawn != null)
                     {
-                        //If they already have plague, add a random range to its severity.
-                        //If severity reaches 1.0f, or 100%, plague kills the target.
+                        // If they already have plague, add a random range to its severity.
+                        // If severity reaches 1.0f, or 100%, plague kills the target.
                         plagueOnPawn.Severity += randomSeverity;
                     }
                     else
                     {
-                        //These three lines create a new health differential or Hediff,
-                        //put them on the character, and increase its severity by a random amount.
-                        Hediff hediff = HediffMaker.MakeHediff(Def.HediffToAdd, hitPawn);
+                        // These three lines create a new health differential or Hediff,
+                        // put them on the character, and increase its severity by a random amount.
+                        Hediff hediff = HediffMaker.MakeHediff(Props.HediffToAdd, hitPawn);
                         hediff.Severity = randomSeverity;
                         hitPawn.health.AddHediff(hediff);
                     }
                 }
-                else //failure!
+                else // failure!
                 {
                     /*
                      * Motes handle all the smaller visual effects in RimWorld.
                      * Dust plumes, symbol bubbles, and text messages floating next to characters.
                      * This mote makes a small text message next to the character.
                      */
-                    MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "TST_PlagueBullet_FailureMote".Translate(Def.AddHediffChance), 12f);
+                    MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "TST_PlagueBullet_FailureMote".Translate(Props.AddHediffChance), 12f);
                 }
             }
         }
